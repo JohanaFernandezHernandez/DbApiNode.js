47a3ade3ece8614ff1da80a23b397593071b430c
JohanaFernandezHernandez <najofe5@gmail.com>
2024-04-25 20:24:44 +0200

    feat: estructura de proyecto

---
+++ config/mongo.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const dbConnect = async () => {
+    try {
+        const DB_URI = process.env.DB_URI;
+        await mongoose.connect(DB_URI, {});
+        console.log("Conexión exitosa a MongoDB");
+    } catch (error) {
+        console.error("Error al conectar a MongoDB:", error.message);
+    }
+};
+
+
+module.exports = dbConnect
+
